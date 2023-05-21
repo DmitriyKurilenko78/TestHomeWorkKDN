a6a8e7997e27d7ce94542ad9ef225955d63763f8
DmitriyKurilenko78 <131363925+DmitriyKurilenko78@users.noreply.github.com>
2023-05-22 01:45:59 +0300

    Решение задач, ввод-вывод данных, if. while. for

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+Console.Clear();            // Очищает консоль
+Console.WriteLine("Hello");  // Выводит данные на одной строчке
+Console.Write(", word!");            // Выводит на той же строчке
+
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+*/
+// ctrl и /
+
+// Ввод чисел
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{n} + {m} = {n + m}");
+
+// желтое выделение - это предупрежденеи
+// красное выделение - это ошибка
+// $"" - это интерполяция строк
+
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// if (n > m)
+//    Console.WriteLine("Первое число больше второго");
+// else if (n < m)
+//    Console.WriteLine("Второе число больше первого");
+// else
+//    Console.WriteLine("Числа равны");
+
+// int i = 1;
+// while (i < 10)
+// {
+//     Console.Write($"{i} ");
+//     i++;
+// }
+
+// int i = 1;
+// do{
+//     Console.Write($"{i} ");
+//     i++;
+// }
+// while (i < 10);
+
+// For
+// for (begin; condition; step)
+
+
+// for (int i = 0; i < 10; i++)
+//      Console.Write($"{i} ");
+
+// Console.WriteLine();
+
+
+// for (int i = 10; i > 0; i--)
+//      Console.Write($"{i} ");
+
+// Console.WriteLine();
+
+
+// for (int i = 0; i < 10; i+=2)
+//      Console.Write($"{i} ");
+
+// Console.WriteLine();
+
+Console.Clear();
+Console.Write("Введите число :");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(n * n);
+
+
