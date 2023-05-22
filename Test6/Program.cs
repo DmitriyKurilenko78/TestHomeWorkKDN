9cc2de850d86e51afd15725a3dc2e01321cb6af0
DmitriyKurilenko78 <131363925+DmitriyKurilenko78@users.noreply.github.com>
2023-05-23 01:26:15 +0300

    13

--- Test6/Program.cs
+++ Test6/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 13: Напишите программу, которая выводит третью  цифру заданного числа или сообщает, 
+//            что третьей цифры нет.  
+//            645 -> 5
+//            78 -> третьей цифры нет
+//            32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+if (n >= 1 && n < 100)
+
+    Console.WriteLine("Третьей цифры нет");
+   
+else if (n >= 100 && n < 999)
+         Console.WriteLine(n % 10); 
+else if (n >= 1000 && n < 9999)
+         Console.WriteLine(n / 10 % 10); 
+else
+    Console.WriteLine(n / 100 % 10);
