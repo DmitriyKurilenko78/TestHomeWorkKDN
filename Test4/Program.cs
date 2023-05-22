679e062a7736767ec5b45aaede2fd06592278adc
DmitriyKurilenko78 <131363925+DmitriyKurilenko78@users.noreply.github.com>
2023-05-23 00:54:51 +0300

    10

--- Test4/Program.cs
+++ Test4/Program.cs
@@ -50,7 +50,20 @@ for (int i = 1; i <= n; i++)
       Console.Write($"{i}, ");
       
       
-      
+      // Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+//            и на выходе показывает вторую цифру этого числа.
+//            456 -> 5
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 100 || n > 999)
+{
+      Console.Write("Вы ввели не верное число!\nВведите число: ");
+       n = int.Parse(Console.ReadLine()!);
+}
+Console.WriteLine(n / 10 % 10);
+
+
       
     
 
