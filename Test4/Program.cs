c2d014536ec6907f2a3f6605d4bb543222fbcd28
DmitriyKurilenko78 <131363925+DmitriyKurilenko78@users.noreply.github.com>
2023-05-22 23:53:05 +0300

    Две задачки

--- Test4/Program.cs
+++ Test4/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("Введите число a = ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число b = ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b)
+    Console.Write($"max = {a}, min = {b}");
+else 
+    Console.Write($"max = {b}, min = {a}");
+
+    
+
+Console.Clear();
+Console.Write("Введите числo a ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите числo b ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите числo c ");
+int c = int.Parse(Console.ReadLine()!);
+
+if (a > b && a > c)
+    Console.Write(a);
+else if (b > a && b > c)    
+    Console.Write(b);
+else 
+    Console.Write(c);
+
+    
+
