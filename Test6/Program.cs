e09b18106eeffc5e05ff74e61f0f04c03b76cd61
DmitriyKurilenko78 <131363925+DmitriyKurilenko78@users.noreply.github.com>
2023-05-23 20:36:14 +0300

    Задача 15

--- Test6/Program.cs
+++ Test6/Program.cs
@@ -25,3 +25,25 @@ Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
 string str = number.ToString();
 Console.WriteLine(str[2]);
+
+
+/////////////////////////
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, 
+//            обозначающую день недели, и проверяет, является ли этот день выходным.
+//            6 -> да
+//            7 -> да
+//            1 -> нет
+Console.Clear();
+Console.Write("Введите число соответствующее дню недели: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 1 || n > 7)
+{
+      Console.WriteLine("Не верное число\nВведите число: ");
+          n = int.Parse(Console.ReadLine()!);  
+}
+if (n >= 1 && n <= 5)
+    Console.Write("Нет");
+else 
+    Console.Write("Да");    
